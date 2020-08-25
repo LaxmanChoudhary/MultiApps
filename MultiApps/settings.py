4a9db05d7ea7562c0b89cc5003b0b6569b8e11dd
Laxman Choudhary <laxmanchoudhary6811@gmail.com>
2020-08-26 00:30:33 +0530

    heroku-db fix

--- MultiApps/settings.py
+++ MultiApps/settings.py
@@ -167,3 +167,9 @@ STATICFILES_DIRS = [
 #for debug
 #print('Sroot', STATIC_ROOT)
 #print('Slist', STATICFILES_DIRS[0])
+
+#for heroku maybe
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)

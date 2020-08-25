f78fcfb82a8e7d9708b33e9d80a429173f6e7bab
Laxman Choudhary <laxmanchoudhary6811@gmail.com>
2020-08-25 11:57:56 +0530

    settings:static

--- MultiApps/settings.py
+++ MultiApps/settings.py
@@ -158,5 +158,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "home", "static"),
 ]
+
+#for debug
+#print('Sroot', STATIC_ROOT)
+#print('Slist', STATICFILES_DIRS[0])
